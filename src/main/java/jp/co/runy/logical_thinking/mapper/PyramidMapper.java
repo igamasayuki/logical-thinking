fdb8f2571472874271f2b25e69d3b51a63c843a1
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2020-11-27 17:23:59 +0900

    不要なインポートの削除

--- src/main/java/jp/co/runy/logical_thinking/mapper/PyramidMapper.java
+++ src/main/java/jp/co/runy/logical_thinking/mapper/PyramidMapper.java
@@ -3,7 +3,6 @@ package jp.co.runy.logical_thinking.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import jp.co.runy.logical_thinking.domain.Pyramid;
 
