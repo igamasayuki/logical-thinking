757fe75f1b14802ebfae9d163431068b24e01366
atsushi-nonaka <atsushi.nonaka@rakus-partners.co.jp>
2020-12-11 15:58:26 +0900

    設定の調整

--- src/main/java/jp/co/runy/logical_thinking/controller/ShowMindMapController.java
+++ src/main/java/jp/co/runy/logical_thinking/controller/ShowMindMapController.java
@@ -3,6 +3,7 @@ package jp.co.runy.logical_thinking.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -34,6 +35,7 @@ public class ShowMindMapController {
 		ObjectMapper mapper = new ObjectMapper();
 		//JSON⇒Javaオブジェクトに変換
 		LogicTree logicTree = mapper.readValue(json, LogicTree.class);	
+		System.out.println(logicTree);
 		model.addAttribute("logicTree", logicTree);
 		return "logicTree/logictree-map.html";
 	}
